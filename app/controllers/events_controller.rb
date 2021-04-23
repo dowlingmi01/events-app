92d72ab1c5a211aea630b34cb53a5ae1da26d4f3
dowlingmi01 <michaeldowling440@gmail.com>
2021-04-23 15:24:27 -0600

    Refactor and unDRY

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,10 @@
 class EventsController < ApplicationController
-
+	before_action :set_event, only: [:show, :edit, :update, :destroy]
 	def index
 		@events = Event.all		
 	end
 
 	def show
-		@event = Event.find(params[:id])
 	end
 
 	def new
@@ -26,11 +25,9 @@ class EventsController < ApplicationController
 
 
 	def edit
-		@event = Event.find(params[:id])
 	end
 
 	def update
-		@event =Event.find(params[:id])
 
 		if @event.update(event_params)
 			flash[:notice] = "Event updated successfully"
@@ -42,13 +39,20 @@ class EventsController < ApplicationController
 	end
 
 	def destroy
-		@event = Event.find(params[:id])
 		@event.destroy
 		flash[:alert] = "Event deleted successfully"
 		redirect_to events_url
 	end
 
 	private
+
+	def set_event
+		@event = Event.find(params[:id])
+
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] #= "The page you requested does not exist"
+		redirect_to events_path
+	end
 
 	def event_params
 		params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
