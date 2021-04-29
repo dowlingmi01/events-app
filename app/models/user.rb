8fc688d2435c2e703a5e3ce37bd6e1d93e6089e8
dowlingmi01 <michaeldowling440@gmail.com>
2021-04-29 16:35:03 -0600

    devise registration views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-	has_many :organized_events, class_name: "Event", dependent: :destroy
+
+    attr_accessor :first_name, :last_name, :user_name
+
+	has_many :organized_events, class_name: "Event", dependent: :destroy	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
